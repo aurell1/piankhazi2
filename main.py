545dc21c7642c5380f49d69cec496783b7671365
aurell1 <burik.aurel@stud.u-szeged.hu>
2016-10-01 15:28:44 +0200

    v0.5

--- main.py
+++ main.py
@@ -1,38 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 import os
 
-app = webapp2.WSGIApplication([
-    ('/', MainPage),
-    ('/sign', Guestbook),
-], debug=True)
-
-class Guestbook(webapp2.RequestHandler):
-
-    def post(self):
-        # We set the same parent key on the 'Greeting' to ensure each
-        # Greeting is in the same entity group. Queries across the
-        # single entity group will be consistent. However, the write
-        # rate to a single entity group should be limited to
-        # ~1/second.
-        guestbook_name = self.request.get('guestbook_name',
-                                          DEFAULT_GUESTBOOK_NAME)
-        greeting = Greeting(parent=guestbook_key(guestbook_name))
-
-        if users.get_current_user():
-            greeting.author = Author(
-                    identity=users.get_current_user().user_id(),
-                    email=users.get_current_user().email())
-
-        greeting.content = self.request.get('content')
-        greeting.put()
-
-        query_params = {'guestbook_name': guestbook_name}
-        self.redirect('/?' + urllib.urlencode(query_params))
+app = Flask(__name__)
 
 @app.route("/")
 def my_func():
-
-	return "Sikeres futas!"
+	return render_template('index.html')
 	
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=os.environ.get("PORT", None))
