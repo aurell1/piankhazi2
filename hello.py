b8ed9a5a65b9e22f2cb7d088598b21cac3039591
aurell1 <burik.aurel@stud.u-szeged.hu>
2016-10-01 17:01:59 +0200

    v0.9

--- hello.py
+++ hello.py
@@ -4,8 +4,12 @@ import os
 app = Flask(__name__)
 
 @app.route("/")
-def my_func():
-	return "Sikeres futas!"
+def home():
+	return render_template('home.html')
+
+@app.route('/about/')
+def about():
+    return render_template('about.html')
 	
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=os.environ.get("PORT", None))
