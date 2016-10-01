87feed1cf899ba67005f81d582b2405b1d26f914
aurell1 <burik.aurel@stud.u-szeged.hu>
2016-10-01 15:20:09 +0200

    v0.4.1

--- main.py
+++ main.py
@@ -1,21 +1,38 @@
 from flask import Flask
 import os
-import web
 
-app = Flask(__name__)
-render = web.template.render('templates/')
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+    ('/sign', Guestbook),
+], debug=True)
+
+class Guestbook(webapp2.RequestHandler):
+
+    def post(self):
+        # We set the same parent key on the 'Greeting' to ensure each
+        # Greeting is in the same entity group. Queries across the
+        # single entity group will be consistent. However, the write
+        # rate to a single entity group should be limited to
+        # ~1/second.
+        guestbook_name = self.request.get('guestbook_name',
+                                          DEFAULT_GUESTBOOK_NAME)
+        greeting = Greeting(parent=guestbook_key(guestbook_name))
+
+        if users.get_current_user():
+            greeting.author = Author(
+                    identity=users.get_current_user().user_id(),
+                    email=users.get_current_user().email())
+
+        greeting.content = self.request.get('content')
+        greeting.put()
+
+        query_params = {'guestbook_name': guestbook_name}
+        self.redirect('/?' + urllib.urlencode(query_params))
 
 @app.route("/")
 def my_func():
 
 	return "Sikeres futas!"
-	
-class Index(object):
-    def GET(self):
-        form = web.input(name="Nobody")
-        greeting = "Hello, %s" % form.name
-
-        return render.index(greeting = greeting)
 	
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=os.environ.get("PORT", None))
