e613e2ceeb4a67415d07240587b7478e6575884c
aurell1 <burik.aurel@stud.u-szeged.hu>
2016-10-01 15:09:39 +0200

    v0.4

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 from flask import Flask
 import os
+import web
 
 app = Flask(__name__)
+render = web.template.render('templates/')
 
 @app.route("/")
 def my_func():
+
 	return "Sikeres futas!"
+	
+class Index(object):
+    def GET(self):
+        form = web.input(name="Nobody")
+        greeting = "Hello, %s" % form.name
+
+        return render.index(greeting = greeting)
 	
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=os.environ.get("PORT", None))
